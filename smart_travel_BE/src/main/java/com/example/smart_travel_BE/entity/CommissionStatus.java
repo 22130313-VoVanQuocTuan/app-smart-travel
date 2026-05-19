0648279e5927bd05ddb22ebffdef025c06cf5061
22130313-VoVanQuocTuan <22130313@st.hcmuaf.edu.vn>
2026-05-19 12:06:46 +0700

    update

---
+++ smart_travel_BE/src/main/java/com/example/smart_travel_BE/entity/CommissionStatus.java
@@ -0,0 +1,7 @@
+package com.example.smart_travel_BE.entity;
+
+public enum CommissionStatus {
+    PENDING,      // Chưa thanh toán
+    PAID,         // Đã thanh toán
+    CANCELLED     // Đã hủy
+}
